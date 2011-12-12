702f5c1401a67072534696286ff3ec2228cb3f1c
Marian Sigler <m@qjym.de>
2011-12-12 03:23:55 +0100

    Pong: test for "PING "... not "PING"...

--- Splirc/Modules/Pong.hs
+++ Splirc/Modules/Pong.hs
@@ -7,7 +7,7 @@ setup :: IO [EventHandler]
 setup = return [OnEveryResponse pongHandler]
 
 pongHandler (IsResponse resp) = 
-    if (map toUpper $ take 4 resp) == "PING" then
+    if (map toUpper $ take 5 resp) == "PING " then
         return [SendCommand $ Pong $ drop 5 resp]
     else
         return []
